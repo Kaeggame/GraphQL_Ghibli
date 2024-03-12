4d71cbb2b18fcdb24634ac8d8fad37edef2b7a95
Flamby <66632621+Kaeggame@users.noreply.github.com>
2024-03-12 16:23:23 +0100

    P'tite correction

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,7 +4,6 @@ import fetch from 'node-fetch';
 const resolvers = {
   Query: {
     async getFilms() {
-      // Appel à l'API REST pour récupérer les films
       const films = await fetch('https://ghibliapi.dev/films').then(res => res.json());
       return films.map(film => ({
         ...film,
@@ -12,7 +11,6 @@ const resolvers = {
       }));
     },
     async getPeople() {
-      // Appel à l'API REST pour récupérer les personnes
       const people = await fetch('https://ghibliapi.dev/people').then(res => res.json());
       return people.map(person => ({
         ...person,
@@ -23,23 +21,20 @@ const resolvers = {
   },
   Film: {
     people(parent) {
-      // Récupérer les informations des personnes à partir de leur ID
       return Promise.all(parent.people.map(async ({ id }) => {
         const person = await fetch(`https://ghibliapi.dev/people/${id}`).then(res => res.json());
         return {
           ...person,
-          eyeColor: person.eye_color || 'N/A' // Fetch the eye color information and return it, or return 'N/A' if not available
+          eyeColor: person.eye_color || 'N/A'
         };
       }));
     }
   },
   People: {
     id(parent) {
-      // Vérifier si l'ID est présent et le renvoyer, sinon renvoyer une valeur par défaut
       return parent.id || 'N/A';
     },
     films(parent) {
-      // Récupérer les informations des films à partir de leur ID
       return parent.films.map(async ({ id }) => {
         return fetch(`https://ghibliapi.dev/films/${id}`).then(res => res.json());
       });
